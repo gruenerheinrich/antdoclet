af04335540b52b15fa3164bd78146e97fb8bcc95
gruenerheinrich <info@centauron.de>
2012-03-23 08:47:02 +0100

    initial

---
+++ src/org/centauron/ant/doclet/AntTaskDoc.java
@@ -0,0 +1,194 @@
+package org.centauron.ant.doclet;
+
+import java.io.OutputStream;
+import java.util.Vector;
+
+import com.sun.javadoc.ClassDoc;
+import com.sun.javadoc.MethodDoc;
+import com.sun.javadoc.ProgramElementDoc;
+
+public class AntTaskDoc extends ResourceWriter  {
+	
+	private AntDoc m_parent;
+	private OutputStream m_stream;
+	private ClassDoc m_doc;
+	public AntTaskDoc(AntDoc parent,ClassDoc doc,OutputStream mystream) {
+		super(parent);
+		m_parent=parent;
+		m_stream=mystream;
+		m_doc=doc;
+	}
+	
+	public void writeout() throws Exception {
+		this.writeHeader();
+		this.writeDeprecated();
+		this.writeDescription();
+		this.writeParameters();
+		this.writeNestedParameters();
+		this.writeExamples();
+		this.writeFooter();
+		
+	}
+	
+	private void writeHeader() throws Exception {
+		Object[] arguments = {
+	             m_parent.getCorrectTaskName(m_doc)		
+		};
+		write(m_stream,"task-header.template",arguments);
+	}
+	
+	private void writeDeprecated() throws Exception {
+		if (m_parent.isDeprecatedTask(m_doc)) {
+			Object[] arguments = {
+		             m_parent.getTagText(m_doc,Constants.TAG_DEPRECATED,true)		
+			};
+			write(m_stream,"task-deprecated.template",arguments);			
+		}
+	}
+
+	private void writeDescription() throws Exception {
+			Object[] arguments = {
+					m_parent.getDescription(m_doc)		
+			};
+			write(m_stream,"task-description.template",arguments);			
+	}
+	private void writeFooter() throws Exception {
+		Object[] arguments = {
+	             Constants.BRANDING		
+		};
+		write(m_stream,"task-footer.template",arguments);
+	}
+	
+	private MethodDoc[] getAllSetParameters(ClassDoc doc) {
+		MethodDoc[] m=m_parent.getDocumentableSetMethods(doc);
+		if (m_parent.getSuperElements().equalsIgnoreCase("add")) {
+			ClassDoc sd=doc.superclass();
+			if (sd!=null) {
+				if (!m_parent.isBaseTask(sd) && m_parent.shouldBeDocumented(sd)) {
+					System.out.println(sd.qualifiedTypeName());
+					MethodDoc[] m2=getAllSetParameters(sd);				
+					m=Utility.concatArray(m,m2);
+				}
+			}
+		}
+		return m;
+		
+	}
+	private void writeParameters() throws Exception {
+		
+		
+		MethodDoc[] m=getAllSetParameters(m_doc);
+		m=Utility.sortArray(m);
+		if (m.length==0) {
+			return;
+		}
+		
+		write(m_stream,"task-parameters-header.template");
+		for (int i=0;i<m.length;i++) {
+			writeSingleParameter(m[i]);
+		}
+		write(m_stream,"task-parameters-footer.template");
+	}
+	
+	private void writeSingleParameter(MethodDoc m) throws Exception {
+		if (m==null) {
+			return;
+		}
+		String name=m_parent.getNameFromSetter(m);
+		String descr=m_parent.getDescription(m);
+		if (m_parent.isOptionalMethod(m)) {
+			//descr=descr+"<br>"+m_parent.getTagText(m, Constants.TAG_OPTIONAL,true);
+			String req=m_parent.getTagText((ProgramElementDoc)m, Constants.TAG_OPTIONAL,true);
+			Object[] arguments = {
+		             name,descr,req		
+			};
+			write(m_stream,"task-parameters-optionalentry.template",arguments);
+			
+		} else {
+			Object[] arguments = {
+		             name,descr		
+			};
+			write(m_stream,"task-parameters-entry.template",arguments);
+		}
+	}
+	
+	private void writeExamples() throws Exception {
+		int i;
+		Vector<String> v=m_parent.getTagTextVector(m_doc, Constants.TAG_EXAMPLE, true);
+		Vector<String> vc=m_parent.getTagTextVector(m_doc, Constants.TAG_EXAMPLE_CODE, true);
+		Vector<String> vd=m_parent.getTagTextVector(m_doc, Constants.TAG_EXAMPLE_DESCRIPTION, true);
+		if (vc.size()!=vd.size()) {
+			throw new Exception("Size of ant.example.code("+vc.size()+") and ant.example.description("+vd.size()+") do not match for Task" + m_doc.qualifiedName()+ ".\n Either all or none of the tags must be localized." );
+		}
+		if (v.size()==0 && vc.size()==0) {
+			//NO EXAMPLES
+			return;
+		}
+		write(m_stream,"task-examples-header.template");
+		
+		for (i=0;i<v.size();i++) {
+			writeSingleExample(v.elementAt(i));	
+		}
+		for (i=0;i<vc.size();i++) {
+			writeSingleDetailedExample(vc.elementAt(i),vd.elementAt(i));
+		}
+		write(m_stream,"task-examples-footer.template");		
+	}
+	private void writeSingleExample(String ex) throws Exception {
+		Object[] arguments = {
+	             ex	
+		};
+		write(m_stream,"task-examples-entry.template",arguments);		
+	}
+	private void writeSingleDetailedExample(String x1,String x2) throws Exception {
+		Object[] arguments = {
+	             Utility.escapeXMLString(x1),
+	             x2
+		};
+		write(m_stream,"task-examples-detailedentry.template",arguments);		
+	}
+	
+	private Vector<String[]> getAllNestedParameters(ClassDoc doc) throws Exception {
+		Vector<String[]> v=m_parent.getSpecialNestedParameters(doc);
+		Vector<String[]> vg=m_parent.getGeneralNestedParameters(doc);		
+		v.addAll(vg);
+		if (doc.superclass()!=null) {
+			Vector<String[]> vs=this.getAllNestedParameters(doc.superclass());
+			v.addAll(vs);
+		}
+		/*
+		if (m_parent.getSuperElements().equalsIgnoreCase("add")) {
+			ClassDoc sd=doc.superclass();
+			if (sd!=null) {
+				if (!m_parent.isBaseTask(sd) && m_parent.shouldBeDocumented(sd)) {
+					System.out.println(sd.qualifiedTypeName());
+					Vector<String[]> v2=this.getAllNestedParameters(sd);				
+					v.addAll(v2);
+				}
+			}
+		}
+		*/		
+		return v;
+	}
+	private void writeNestedParameters() throws Exception {
+		Vector<String[]> v=getAllNestedParameters(m_doc);
+		if (v.size()==0) return;
+		write(m_stream,"task-nestedparameters-header.template");
+		for (int i=0;i<v.size();i++) {
+			String[] sar=v.elementAt(i);
+			//System.out.println("NESTEDPARAMETER:"+sar[0]);
+			//System.out.println("NESTEDPARAMETER-DESCRIPTION:"+sar[1]);
+			writeSingleNestedParameter(sar[0],sar[1]);
+		}
+		write(m_stream,"task-nestedparameters-footer.template");
+		
+	}
+	
+	private void writeSingleNestedParameter(String x1,String x2) throws Exception {
+			Object[] arguments = {
+		             x1,x2	
+			};
+			write(m_stream,"task-nestedparameters-entry.template",arguments);				
+	}
+
+}
---
+++ src/org/centauron/ant/doclet/Utility.java
@@ -0,0 +1,75 @@
+package org.centauron.ant.doclet;
+
+import java.io.File;
+import java.util.Iterator;
+import java.util.StringTokenizer;
+import java.util.TreeMap;
+
+import org.apache.commons.lang.*;
+
+import com.sun.javadoc.MethodDoc;
+public class Utility {
+	public static void deleteAllFilesInDir(File dir) {
+		File[] files = dir.listFiles();
+		for (File file : files)
+		{
+			if (file.isDirectory()) {
+				Utility.deleteAllFilesInDir(file);
+			}
+			if (!file.delete())
+			{
+				System.out.println("Failed to delete "+file);
+			}
+		}
+	}
+
+
+	public static boolean isMultiLine(String s) {
+		return s.indexOf("\n")!=-1;
+	}
+	public static String escapeXMLString(String xml) {
+		return StringEscapeUtils.escapeXml(xml);
+	}
+
+	public static String lowerFirstCharacter(String s) {
+		if (s==null) return null;
+		if (s.length()==0) return s;
+		return s.substring(0, 1).toLowerCase()+s.substring(1);		
+	}
+	public static String getStringPart(String s,int idx) {
+		StringTokenizer tok=new StringTokenizer(s," ");
+		if (tok.countTokens()<=idx) return null;
+		for (int i=0;i<idx;i++) {
+			tok.nextToken();
+		}
+		return tok.nextToken();
+	}
+
+
+	public static MethodDoc[] concatArray(MethodDoc[] m, MethodDoc[] m2) {
+		MethodDoc a[]=new MethodDoc[m.length+m2.length];
+		for (int i=0;i<m.length;i++) {
+			a[i]=m[i];
+		}
+		for (int i=0;i<m2.length;i++) {
+			a[i+m.length]=m2[i];
+		}
+		return a;
+	}
+	public static MethodDoc[] sortArray(MethodDoc[] m) {
+		MethodDoc[] a=new MethodDoc[m.length];
+		TreeMap<String,MethodDoc> t=new TreeMap();
+		for (int i=0;i<m.length;i++) {
+			t.put(m[i].name(), m[i]);
+		}
+		int i=0;
+		Iterator<MethodDoc> it=t.values().iterator();
+		while (it.hasNext()) {
+			a[i]=it.next();
+			++i;
+		}
+		return a;
+	}
+	
+}
+
