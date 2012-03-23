c29eeecb7ca76889da88e41645db0937d60c8594
gruenerheinrich <info@centauron.de>
2012-03-23 09:21:32 +0100

    initial

---
+++ src/org/centauron/ant/doclet/StaticResourceDoc.java
@@ -0,0 +1,37 @@
+package org.centauron.ant.doclet;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.io.InputStream;
+import java.text.*;
+
+/**
+ * Antdoc doclet
+ *
+ * Copyright: Copyright (c) 2002, Toni Thomsson, All rights reserved.
+ * @author centauron
+ * @version 0.5
+ */
+public class StaticResourceDoc extends ResourceWriter
+{
+	public StaticResourceDoc(AntDoc pa) {
+		super(pa);
+	}
+	public void writeFile(String fname,String newfilename,Object[] args) throws Exception {
+		//CHECK IF DIR FOR newfileexists
+		String nf=m_parent.getOutput() + File.separatorChar + newfilename;
+		File f=new File(nf);
+		f.getParentFile().mkdirs();
+		FileOutputStream out=new FileOutputStream( nf);
+		write(out,fname,args);
+	};
+	public void writeFile(String fname,String newfilename) throws Exception {
+		writeFile(fname,newfilename,null);
+	};
+	
+	public void writeFile(String fname) throws Exception {		
+		writeFile(fname,fname);
+	};
+	
+}
