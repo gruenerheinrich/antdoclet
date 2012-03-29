fcf030c300d54f7249959abf2e63ffa47201157d
gruenerheinrich <info@centauron.de>
2012-03-29 09:55:10 +0200

    Examples

---
+++ examples/src/org/centauron/ant/doclet/examples/HelloWorldChildTask.java
@@ -0,0 +1,19 @@
+package org.centauron.ant.doclet.examples;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Task;
+
+/**
+ * @ant.description
+ * If no localisation is found, than the java standard locale fallback is used. 
+ *
+ * @author centauron - Stefan Bernsdorf
+ * @version 0.7
+ */
+public class HelloWorldChildTask extends Task {
+
+    public void execute() throws BuildException {
+    }
+    
+}
+
